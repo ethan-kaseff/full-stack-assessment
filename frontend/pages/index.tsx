045e7627615ffb8cdbfd78e6ea17f05e09f33eab
Ethan Kaseff <ethan@kaseff.com>
2022-03-22 13:41:07 -0500

    add link to Portfolio, and setup space to write code

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -12,7 +12,7 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
+      {/* <main className={styles.main}>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
@@ -51,18 +51,15 @@ const Home: NextPage = () => {
             </p>
           </a>
         </div>
-      </main>
+      </main> */}
 
       <footer className={styles.footer}>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="https://ethan-kaseff.github.io/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
+          Developed by Ethan Kaseff
         </a>
       </footer>
     </div>
